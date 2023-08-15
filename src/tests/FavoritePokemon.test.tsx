725470da5443c267e562e96503ffd94b68f16ba4
esyamamoto <erika.s.yamamoto@gmail.com>
2023-08-15 19:18:57 -0300

    req  3 e 5 tentando

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,16 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import { FavoritePokemon } from '../pages';
+/*
+import { screen } from '@testing-library/react';
 import App from '../App';
+import renderWithRouter from '../renderWithRouter';
 
 describe(' Teste o componente <FavoritePokemon.tsx />', () => {
   it('É exibido na tela a mensagem No favorite pokemon found', () => {
-    render(<FavoritePokemon />, { wrapper: BrowserRouter });
-    const notFound = /No favorite pokemon found/i;
-    expect(screen.getByText('notFound')).toBeInTheDocument();
+    renderWithRouter(<App />, { route: '/favorites' });
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
   });
+
   // ----------------------------------------------------------------------------------------------------------------------------
-  it('São exibidos na tela apenas os Pokémon favoritados', () => {
-    render(<FavoritePokemon />, { wrapper: BrowserRouter });
+
+  it('São exibidos na tela apenas os Pokémon favoritados', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/pokemon/151' });
+
+    const favorite = screen.getByRole('checkbox', { name: /Pokémon favoritados/i });
+    await user.click(favorite);
+
+    const linkFavorite = screen.getByRole('link', { name: /favorite pokemon/i });
+    await user.click(linkFavorite);
+
+    const favoritePokemon = screen.getByText(/Mew/i);
+    expect(favoritePokemon).toBeInTheDocument();
   });
 });
+*/
--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,60 @@
-test('', () => {});
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event/dist/types/setup';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <Pokedex.tsx />', () => {
+  it('Teste se a página contém um heading h2 com o texto Encountered Pokémon.', () => {
+    renderWithRouter(<App />, { route: '/' });
+    const about = /Encountered Pokémon/i;
+    expect(screen.getByRole('heading', { name: about })).toBeInTheDocument();
+  });
+
+  // ----------------------------------------------------------------------------------------------------------------------------
+  it('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/' });
+
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName).toBeInTheDocument();
+
+    const buttonList = screen.getByRole('button', { name: 'Próximo Pokémon' });
+    expect(buttonList).toBeInTheDocument();
+
+    await user.click(buttonList);
+    expect(pokemonName.textContent).toBe('Charmander');
+    await user.click(buttonList);
+    expect(pokemonName.textContent).toBe('Caterpie');
+    await user.click(buttonList);
+    expect(pokemonName.textContent).toBe('Ekans');
+    await user.click(buttonList);
+    expect(pokemonName.textContent).toBe('Alakazam');
+    await user.click(buttonList);
+    expect(pokemonName.textContent).toBe('Mew');
+  });
+
+  // ----------------------------------------------------------------------------------------------------------------------------
+  it('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(<App />);
+    const pokemon = screen.queryAllByTestId('pokemon-name');
+    expect(pokemon).toHaveLength(1);
+  });
+
+  // ----------------------------------------------------------------------------------------------------------------------------
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const buttonFilterPokemon = screen.getAllByTestId('pokemon-type-button');
+    expect(buttonFilterPokemon).toHaveLength(7);
+    // expect.arrayContaining(['Electric', 'Bug', 'Poison', 'Psychic', 'Fire', 'Normal', 'Dragon']));
+  });
+
+  // ----------------------------------------------------------------------------------------------------------------------------
+  it('Teste se a Pokédex contém um botão para resetar o filtro', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/' });
+    const reseteButton = screen.getByRole('button', { name: /All/i });
+    expect(reseteButton).toBeInTheDocument();
+    await user.click(reseteButton);
+    const buttonList = /Próximo Pokémon/i;
+    expect(screen.getByRole('button', { name: buttonList })).toBeInTheDocument();
+  });
+});
